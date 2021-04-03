55cac7cc7c76fc3df38335c3cc84a5ec0d519174
adiu <adiu_1024@163.com>
2021-04-03 16:49:53 +0800

    Optimize code

--- es/index.js
+++ es/index.js
@@ -21,37 +21,33 @@ class Download {
       credentials: 'same-origin',
       ...aside
     }).then(response => {
-      const { ok, status, statusText } = response
+      const { ok, status, statusText, headers } = response
       if (!ok) {
         return Promise.reject({ status, statusText })
+      } else {
+        filename = filename || decodeURI(headers.get('Content-Disposition').split('filename=')[1])
+        return response
+      }
+    }).then(async response => {
+      if (typeof getProgress !== 'function') {
+        try {
+          const { status, msg } = await response.clone().json()
+          return Promise.reject({ status, statusText: msg })
+        } catch (error) {
+          this.saveAs(await response.clone().blob(), filename)
+          return Promise.reject({ status: 200, statusText: 'No need to get download progress.'})
+        }
       } else {
         return response
       }
     }).then(async response => {
-      try {
-        if (aside.method === 'POST') {
-          const { status, msg } = await response.clone().json()
-          return Promise.reject({ status, statusText: msg })
-        } else {
-          return response
-        }
-      } catch (error) {
-        filename = filename || decodeURI(response['headers'].get('Content-Disposition').split('filename=')[1])
-        return response.clone()
+      const { headers, body: stream } = response
+      const totalSize = headers.get('Content-Length') || 0
+      if (totalSize === 0) {
+        return Promise.reject({ status: 400, statusText: 'Missing file size for Content-Length field.'})
       }
-    }).then(async response => {
-      if (typeof getProgress !== 'function') {
-        this.saveAs(await response.blob(), filename)
-        return Promise.reject({ status: 200, statusText: 'No need to get download progress.'})
-      } else {
-        const { headers, body: stream } = response
-        const totalSize = headers.get('Content-Length') || 0
-        if (totalSize === 0) {
-          return Promise.reject({ status: 400, statusText: 'Missing file size for Content-Length field.'})
-        }
-        const reader = stream.getReader()
-        return { totalSize, reader }
-      }
+      const reader = stream.getReader()
+      return { totalSize, reader }
     }).then(async ({ totalSize, reader }) => {
       let receiveSize = 0, chunks = []
       while(true) {
